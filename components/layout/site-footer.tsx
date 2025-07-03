9e738362e8026d5aba0075493c21ef5bd759f858
Patrick M <patrick.v.murray@gmail.com>
2025-07-03 13:06:58 -0500

    fixed footer icon spacing

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -6,7 +6,7 @@ export function SiteFooter() {
   return (
     <footer className="border-t py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 ml-4">
           <HomeIcon className="h-5 w-5" />
           <p className="text-sm text-muted-foreground">
             © {new Date().getFullYear()} In-House. All rights reserved.
